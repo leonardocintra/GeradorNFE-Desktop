642687d82a6425f73435fad5ffba30ba696aa3a5
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-08-20 20:25:24 -0300

    Add EmitenteBLL and Util Class

---
+++ GeradorNotaFiscal/GeradorNFE.BLL/EmitenteBLL.cs
@@ -0,0 +1,70 @@
+﻿using GeradorNFE.DAO;
+using GeradorNFE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNFE.BLL
+{
+    public class EmitenteBLL
+    {
+
+        public void ExcluirEmitente(Emitente emitente)
+        {
+            if (emitente.EmitenteId > 0)
+                EmitenteDAO.DeleteEmitente(emitente);
+            else
+                throw new Exception("ID do emitente inválido para exclusão");
+        }
+
+        public void CadastrarEmitente(Emitente emitente)
+        {
+            ValidaInformacoesEmitente(emitente);
+            EmitenteDAO.SetEmitente(emitente);
+        }
+
+        public void AtualizarEmitente(Emitente emitente)
+        {
+            ValidaInformacoesEmitente(emitente);
+            EmitenteDAO.UpdateEmitente(emitente);
+        }
+
+        public List<Emitente> BuscarEmitente()
+        {
+            return EmitenteDAO.GetEmitente();
+        }
+
+        public Emitente BuscarEmitenteById(int id)
+        {
+            if (id > 0)
+                return EmitenteDAO.GetEmitenteById(id);
+            else
+                return null;
+        }
+
+        public List<Emitente> BuscarEmitenteComParametro(string parametro)
+        {
+            if (parametro != string.Empty)
+                return EmitenteDAO.GetEmitenteByParameter(parametro);
+            else
+                return EmitenteDAO.GetEmitente();
+        }
+
+        private void ValidaInformacoesEmitente(Emitente emitente)
+        {
+            if (!Util.ValidaCNPJ.IsCnpj(emitente.CNPJ.ToString()))
+                throw new Exception("CNPJ inválido! Verifique e tente novamente");
+
+            if (emitente.NomeRazao == string.Empty)
+                throw new Exception("Nome Razão é obrigatório");
+
+            if (emitente.Logradouro == string.Empty)
+                throw new Exception("Endereço/Logradouro é obrigatório!");
+
+            if (emitente.Cidade == string.Empty)
+                throw new Exception("Nome da cidade é obrigatório");
+        }
+    }
+}
---
+++ GeradorNotaFiscal/GeradorNFE.BLL/Util.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNFE.BLL
+{
+    public class Util
+    {
+
+        // validação extraida de: http://www.macoratti.net/11/09/c_val1.htm
+        /// <summary>
+        /// Realiza a validação do CNPJ
+        /// </summary>
+        public static class ValidaCNPJ
+        {
+            public static bool IsCnpj(string cnpj)
+            {
+                int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int soma;
+                int resto;
+                string digito;
+                string tempCnpj;
+                cnpj = cnpj.Trim();
+                cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+                if (cnpj.Length != 14)
+                    return false;
+                tempCnpj = cnpj.Substring(0, 12);
+                soma = 0;
+                for (int i = 0; i < 12; i++)
+                    soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+                resto = (soma % 11);
+                if (resto < 2)
+                    resto = 0;
+                else
+                    resto = 11 - resto;
+                digito = resto.ToString();
+                tempCnpj = tempCnpj + digito;
+                soma = 0;
+                for (int i = 0; i < 13; i++)
+                    soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+                resto = (soma % 11);
+                if (resto < 2)
+                    resto = 0;
+                else
+                    resto = 11 - resto;
+                digito = digito + resto.ToString();
+                return cnpj.EndsWith(digito);
+            }
+        }
+
+        /// <summary>
+        /// Realiza a validação do CPF
+        /// </summary>
+        public static class ValidaCPF
+        {
+            public static bool IsCpf(string cpf)
+            {
+                int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                string tempCpf;
+                string digito;
+                int soma;
+                int resto;
+                cpf = cpf.Trim();
+                cpf = cpf.Replace(".", "").Replace("-", "");
+                if (cpf.Length != 11)
+                    return false;
+                tempCpf = cpf.Substring(0, 9);
+                soma = 0;
+
+                for (int i = 0; i < 9; i++)
+                    soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+                resto = soma % 11;
+                if (resto < 2)
+                    resto = 0;
+                else
+                    resto = 11 - resto;
+                digito = resto.ToString();
+                tempCpf = tempCpf + digito;
+                soma = 0;
+                for (int i = 0; i < 10; i++)
+                    soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+                resto = soma % 11;
+                if (resto < 2)
+                    resto = 0;
+                else
+                    resto = 11 - resto;
+                digito = digito + resto.ToString();
+                return cpf.EndsWith(digito);
+            }
+        }
+
+        /// <summary>
+        /// Realiza a validação do número PIS
+        /// </summary>
+        public static class ValidaPis
+        {
+            public static bool IsPis(string pis)
+            {
+                int[] multiplicador = new int[10] { 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int soma;
+                int resto;
+                if (pis.Trim().Length != 11)
+                    return false;
+                pis = pis.Trim();
+                pis = pis.Replace("-", "").Replace(".", "").PadLeft(11, '0');
+
+                soma = 0;
+                for (int i = 0; i < 10; i++)
+                    soma += int.Parse(pis[i].ToString()) * multiplicador[i];
+                resto = soma % 11;
+                if (resto < 2)
+                    resto = 0;
+                else
+                    resto = 11 - resto;
+                return pis.EndsWith(resto.ToString());
+            }
+        }
+    }
+}
