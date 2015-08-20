84e87d43c302753d69809de3063cd260e42cb760
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-08-20 20:18:27 -0300

    Criado class EmitenteDAO and mdf file

---
+++ GeradorNotaFiscal/GeradorNFE.DAO/AbstractCrud.cs
@@ -0,0 +1,40 @@
+﻿using GeradorNFE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNFE.DAO
+{
+    public abstract class AbstractCrud
+    {
+
+        public static void Add<E>(E entity) where E : class
+        {
+            using (GeradorEntities db = new GeradorEntities())
+            {
+                db.Entry(entity).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public static void Update<E>(E entity) where E : class
+        {
+            using (GeradorEntities db = new GeradorEntities())
+            {
+                db.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public static void Delete<E>(E entity) where E : class
+        {
+            using (GeradorEntities db = new GeradorEntities())
+            {
+                db.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
