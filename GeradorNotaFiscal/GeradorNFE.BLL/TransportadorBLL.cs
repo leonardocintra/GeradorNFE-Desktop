766cae4d119abdf863d3534f7fb51bd8c495a72e
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-08-20 20:31:24 -0300

    Generate Model Transportador with DAO and BLL

---
+++ GeradorNotaFiscal/GeradorNFE.BLL/TransportadorBLL.cs
@@ -0,0 +1,88 @@
+﻿using GeradorNFE.DAO;
+using GeradorNFE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNFE.BLL
+{
+    public class TransportadorBLL
+    {
+
+        public void ExcluirTransportador(Transportador transportador)
+        {
+            if (transportador.TransportadorId > 0)
+                TransportadorDAO.DeleteTransportador(transportador);
+            else
+                throw new Exception("ID do transportador inválido para exclusão");
+        }
+
+        public void CadastrarTransportador(Transportador transportador)
+        {
+            ValidaInformacoesTransportador(transportador);
+            TransportadorDAO.SetTransportador(transportador);
+        }
+
+        public void AtualizarTransportador(Transportador transportador)
+        {
+            ValidaInformacoesTransportador(transportador);
+            TransportadorDAO.UpdateTransportador(transportador);
+        }
+
+        public List<Transportador> BuscarTransportador()
+        {
+            return TransportadorDAO.GetTransportador();
+        }
+
+        public Transportador BuscarTransportadorById(int id)
+        {
+            if (id > 0)
+                return TransportadorDAO.GetTransportadorById(id);
+            else
+                return null;
+        }
+
+        public List<Transportador> BuscarTransportadorComParametro(string parametro)
+        {
+            if (parametro != string.Empty)
+                return TransportadorDAO.GetTransportadorByParameter(parametro);
+            else
+                return TransportadorDAO.GetTransportador();
+        }
+
+        private void ValidaInformacoesTransportador(Transportador transportador)
+        {
+            string cnpjCpf = transportador.CNPJCPF.ToString()
+                .Replace(".", string.Empty)
+                .Replace("-", string.Empty)
+                .Replace("/", string.Empty)
+                .Trim();
+
+            if (cnpjCpf.Length.Equals(11))
+            {
+                if (!Util.ValidaCPF.IsCpf(transportador.CNPJCPF.ToString()))
+                    throw new Exception("CPF inválido! Verifique e tente novamente");
+            }
+            else if (cnpjCpf.Length.Equals(14))
+            {
+                if (!Util.ValidaCNPJ.IsCnpj(transportador.CNPJCPF.ToString()))
+                    throw new Exception("CNPJ inválido! Verifique e tente novamente");
+            }
+            else
+            {
+                throw new Exception("CNPJ / CPF inválido! Verifique e tente novamente");
+            }
+
+            if (transportador.NomeRazao == string.Empty)
+                throw new Exception("Nome Razão é obrigatório");
+
+            if (transportador.Endereco == string.Empty)
+                throw new Exception("Endereço/Logradouro é obrigatório!");
+
+            if (transportador.Cidade == string.Empty)
+                throw new Exception("Nome da cidade é obrigatório");
+        }
+    }
+}
---
+++ GeradorNotaFiscal/GeradorNFE.DAO/TransportadorDAO.cs
@@ -0,0 +1,93 @@
+﻿using GeradorNFE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNFE.DAO
+{
+    public class TransportadorDAO
+    {
+
+        public static void DeleteTransportador(Transportador transportador)
+        {
+            try
+            {
+                AbstractCrud.Delete(transportador);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao excluir transportador. Erro: " + ex.Message);
+            }
+        }
+
+        public static void UpdateTransportador(Transportador transportador)
+        {
+            try
+            {
+                AbstractCrud.Update(transportador);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao atualizar o transportador: " + ex.Message);
+            }
+        }
+
+        public static void SetTransportador(Transportador transportador)
+        {
+            try
+            {
+                AbstractCrud.Add(transportador);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao cadatrar o Transportador: " + ex.Message);
+            }
+        }
+
+        public static List<Transportador> GetTransportador()
+        {
+            List<Transportador> _return = new List<Transportador>();
+
+            using (GeradorEntities context = new GeradorEntities())
+            {
+                return _return = context.Transportador.ToList();
+            }
+        }
+
+        public static Transportador GetTransportadorById(int id)
+        {
+            try
+            {
+                Transportador transportador = new Transportador();
+                GeradorEntities db = new GeradorEntities();
+
+                transportador = db.Transportador.Where(e => e.TransportadorId.Equals(id)).FirstOrDefault();
+
+                return transportador;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao buscar transportador. \nErro: " + ex.Message);
+            }
+        }
+
+        public static List<Transportador> GetTransportadorByParameter(string parameter)
+        {
+            try
+            {
+                List<Transportador> listTransportador = new List<Transportador>();
+                GeradorEntities db = new GeradorEntities();
+
+                listTransportador = db.Transportador.Where(e => e.NomeRazao.Contains(parameter)).ToList();
+                return listTransportador;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao buscar Transportador. \nErro: " + ex.Message);
+            }
+
+        }
+    }
+}
