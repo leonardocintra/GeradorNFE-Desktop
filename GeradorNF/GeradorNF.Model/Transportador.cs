75bcd9ee7a69f143a9aa4cad5cc946d2bcc10617
Leonardo Cintra <leonardo.ncintra@Outlook.com>
2016-08-02 07:56:08 -0300

    add model Transportador

---
+++ GeradorNF/GeradorNF.Model/Transportador.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorNF.Model
+{
+    public class Transportador
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("frete_por_conta")]
+        public bool FretePorConta { get; set; }
+
+        [JsonProperty("cpf_cnpj")]
+        public string CPF_CNPJ { get; set; }
+
+        [JsonProperty("inscricao_estadual")]
+        public string InscricaoEstadual { get; set; }
+
+        [JsonProperty("nome_razao")]
+        public string NomeRazao { get; set; }
+
+        [JsonProperty("endereco")]
+        public string Endereco { get; set; }
+
+        [JsonProperty("cidade_codigo")]
+        public int? CidadeCodigo { get; set; }
+
+        [JsonProperty("cidade")]
+        public string Cidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string UF { get; set; }
+
+        [JsonProperty("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
