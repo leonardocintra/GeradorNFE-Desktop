a656a496740afe76c1f09ad14ce04b7de409c9dd
Leonardo <leonardo.ncintra@Outlook.com>
2016-08-03 15:40:37 -0300

    refatorado o GET

--- GeradorNF/GeradorNF.DAO/AbstractCrud.cs
+++ GeradorNF/GeradorNF.DAO/AbstractCrud.cs
@@ -10,6 +10,27 @@ namespace GeradorNF.DAO
 {
     public static class AbstractCrud
     {
+        public static async Task<List<E>> GetAll<E>(string entityURL) where E: class
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(UtilDAO.UrlApi());
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("/" + entityURL);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<List<E>>(json).ToList();
+                }
+                else
+                    return null;
+            }
+        }
+
+
         public static async Task<HttpResponseMessage> Add<E>(E entity, string entityURL) where E : class
         {
             using (var client = new HttpClient())
--- GeradorNF/GeradorNF.DAO/EmitenteDAO.cs
+++ GeradorNF/GeradorNF.DAO/EmitenteDAO.cs
@@ -20,26 +20,7 @@ namespace GeradorNF.DAO
         {
             try
             {
-                List<Emitente> _return = new List<Emitente>();
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri(UtilDAO.UrlApi());
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                    HttpResponseMessage response = await client.GetAsync("/emitente");
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string json = await response.Content.ReadAsStringAsync();
-                        _return = JsonConvert.DeserializeObject<List<Emitente>>(json).ToList();
-                    }
-                    else
-                    {
-                        throw new Exception("Erro ao buscar os Emitentes. Erro:" + response.RequestMessage);
-                    }
-                }
-
-                return _return;
+                return await AbstractCrud.GetAll<Emitente>("emitente");
             }
             catch (JsonException ex)
             {
